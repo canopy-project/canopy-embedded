7751802d49bf86341f8a525452e50175fe14c3ae
Greg Prisament <greg@toruslabs.com>
2014-04-17 20:48:22 -0700

    Add test_haive.c

---
+++ embedded/libhaive/tests/test_haive/test_haive.c
@@ -0,0 +1,26 @@
+/* Copyright 2014 - Greg Prisament
+ */
+
+#include "haive.h"
+#include "red_test.h"
+
+int main(int argc, const char *argv[])
+{
+    RedTest suite = RedTest_Begin(argv[0], NULL, NULL);
+
+    /* Test creation & destruction */
+    {
+        HaiveContext haive = haive_init();
+        RedTest_Verify(suite, "Initialize haive", haive);
+        
+        haive_shutdown(haive);
+        RedTest_Verify(suite, "Shutdown haive (didn't crash)", true);
+    }
+
+    /* Test sddl file loading */
+    {
+        //HaiveContext haive = haive_init();
+        //bool haive_load_device_description(HaiveContext haive, const char *filename);
+    }
+    return 0;
+}
