1b75cf2ac2a7a59254fd83317a94f2cc08a27dc8
Greg Prisament <gregjp@toruslabs.com>
2014-05-05 23:39:53 -0700

    Rename test_haive to test_canopy

---
+++ libcanopy/tests/test_canopy/test_canopy.c
@@ -0,0 +1,34 @@
+/* Copyright 2014 - Greg Prisament
+ */
+
+#include "canopy.h"
+#include "red_test.h"
+
+int main(int argc, const char *argv[])
+{
+    RedTest suite = RedTest_Begin(argv[0], NULL, NULL);
+
+    /* Test creation & destruction */
+    {
+        CanopyContext canopy = canopy_init();
+        RedTest_Verify(suite, "Initialize canopy", canopy);
+        
+        canopy_shutdown(canopy);
+        RedTest_Verify(suite, "Shutdown canopy (didn't crash)", true);
+    }
+
+    /* Test sddl file loading */
+    {
+        CanopyReport report;
+        CanopyContext canopy = canopy_init();
+        canopy_load_device_description(canopy, "mydevice.sddl");
+
+        report = canopy_begin_report(canopy);
+        canopy_report_float32(report, "temperature", 24.38f);
+        canopy_report_float32(report, "humidity", 0.01f);
+        canopy_send_report(report);
+
+        canopy_shutdown(canopy);
+    }
+    return RedTest_End(suite);
+}
