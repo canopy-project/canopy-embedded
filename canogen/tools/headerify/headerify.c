58f866acacde8b2da02f068cbc03fa41e9b415c5
Greg Prisament <gregjp@toruslabs.com>
2014-06-03 16:19:04 -0700

    Add headerify tool for converting file into C string constant

---
+++ canogen/tools/headerify/headerify.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int c;
+    printf("static const char * %s = [\n\"", argv[1]);
+    while ((c = getchar()) != EOF) 
+    {
+        switch ((char)c) {
+            case '"':
+                printf("\\\"");
+                break;
+            case '\\':
+                printf("\\\\");
+                break;
+            case '\n':
+                printf("\",\n\"");
+                break;
+            default:
+                putchar(c);
+                break;
+        }
+    }
+    printf("\"];\n");
+    return 0;
+}
