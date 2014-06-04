01de9de9233df6bab85d35bd5fe5644926336956
Greg Prisament <gregjp@toruslabs.com>
2014-06-03 19:01:46 -0700

    Work on canogen for fan demo

--- canogen/src/canogen.c
+++ canogen/src/canogen.c
@@ -1,12 +1,15 @@
 #include "sddl.h"
+#include <stdio.h>
 #include "canopy_boilerplate_head.out.h"
+#include "canopy_boilerplate_tail.out.h"
 #include <string.h>
 
 
-static bool _dump_boilerplate()
+static bool _dump_boilerplate(SDDLClass cls)
 {
     FILE *fp;
     int i;
+    int numProperties;
     fp = fopen("canopy_boilerplate.h", "w+");
     if (!fp)
     {
@@ -17,6 +20,57 @@ static bool _dump_boilerplate()
     {
         fprintf(fp, "%s\n", _CANOPY_BOILERPLATE_HEAD[i]);
     }
+
+    /* Control callback prototypes */
+    numProperties = sddl_class_num_properties(cls);
+    for (i = 0; i < numProperties; i++)
+    {
+        SDDLProperty prop;
+        prop = sddl_class_property(cls, i);
+        if (sddl_is_control(prop))
+        {
+            const char *controlName = sddl_control_name(SDDL_CONTROL(prop));
+            const char *controlCType = "int8_t";
+
+            fprintf(fp, "static bool on_change__%s(%s value);\n",
+                    controlName,
+                    controlCType);
+        }
+    }
+
+    /* Implement dispatch reoutine */
+    fprintf(fp, "static void dispatch(CanopyEventDetails event)\n{\n");
+
+    numProperties = sddl_class_num_properties(cls);
+    for (i = 0; i < numProperties; i++)
+    {
+        SDDLProperty prop;
+        prop = sddl_class_property(cls, i);
+        if (sddl_is_control(prop))
+        {
+            const char *controlName = sddl_control_name(SDDL_CONTROL(prop));
+            const char *controlCType = "int8_t";
+            const char *controlAbbrevType = "i8";
+
+            fprintf(fp,
+                "    if (canopy_event_control_name_matches(event, \"%s\"))\n"
+                "    {\n"
+                "        %s val;\n"
+                "        canopy_event_get_control_value_%s(event, &val);\n"
+                "        on_change__%s(val);\n"
+                "    }\n",
+                controlName,
+                controlCType,
+                controlAbbrevType,
+                controlName);
+        }
+    }
+    fprintf(fp, "}\n\n");
+
+    for (i = 0; i < sizeof(_CANOPY_BOILERPLATE_TAIL)/sizeof(_CANOPY_BOILERPLATE_TAIL[0]); i++)
+    {
+        fprintf(fp, "%s\n", _CANOPY_BOILERPLATE_TAIL[i]);
+    }
     return true;
 }
 
@@ -24,12 +78,17 @@ static bool _dump_class_control_callbacks(SDDLClass cls)
 {
     FILE *fp;
     unsigned numProperties, i;
-    fp = fopen("stubz.c", "w+");
+    char filename[512];
+    snprintf(filename, 512, "%s.c", sddl_class_name(cls));
+    fp = fopen(filename, "w+");
     if (!fp)
     {
         fprintf(stderr, "Could not open stubz.h for write!");
         return false;
     }
+    fprintf(fp, "#include \"canopy_boilerplate.h\"\n");
+    fprintf(fp, "#include <canopy.h>\n");
+    fprintf(fp, "\n");
     numProperties = sddl_class_num_properties(cls);
     for (i = 0; i < numProperties; i++)
     {
@@ -42,14 +101,19 @@ static bool _dump_class_control_callbacks(SDDLClass cls)
         else if (sddl_is_control(prop))
         {
             const char *propName = sddl_control_name(SDDL_CONTROL(prop));
-            fprintf(fp, "bool on_change__%s(uint32_t value)\n{\n}\n\n", propName);
+            fprintf(fp, "static bool on_change__%s(int8_t value)\n"
+                "{\n"
+                "   /* Your code here.\n"
+                "    * Return true on success.\n"
+                "    */\n"
+                "   return false;\n}\n\n", propName);
         }
     }
-    fprintf(fp, "bool on_canopy_init()\n{\n}\n\n");
-    fprintf(fp, "bool on_canopy_shutdown()\n{\n}\n\n");
-    fprintf(fp, "bool on_connected()\n{\n}\n\n");
-    fprintf(fp, "bool on_disconnected()\n{\n}\n\n");
-    fprintf(fp, "bool on_report_requested()\n{\n}\n\n");
+    fprintf(fp, "static bool on_canopy_init()\n{\n    return false;\n}\n\n");
+    fprintf(fp, "static bool on_canopy_shutdown()\n{\n    return false;\n}\n\n");
+    fprintf(fp, "static bool on_connected()\n{\n    return false;\n}\n\n");
+    fprintf(fp, "static bool on_disconnected()\n{\n    return false;\n}\n\n");
+    fprintf(fp, "static bool on_report_requested()\n{\n    return false;\n}\n\n");
     fclose(fp);
     return true;
 }
@@ -59,8 +123,6 @@ int main(int argc, const char *argv[])
     SDDLDocument doc;
     unsigned numProperties, i;
     doc = sddl_load_and_parse(argv[1]);
-
-    _dump_boilerplate();
 
     numProperties = sddl_document_num_properties(doc);
     for (i = 0; i < numProperties; i++)
@@ -73,6 +135,7 @@ int main(int argc, const char *argv[])
             name = sddl_class_name(SDDL_CLASS(prop));
             if ((argc > 2 && !strcmp(name, argv[2]) )|| argc <= 2)
             {
+                _dump_boilerplate(SDDL_CLASS(prop));
                 _dump_class_control_callbacks(SDDL_CLASS(prop));
             }
         }
---
+++ examples/fan/canopy.example.fan.c
@@ -0,0 +1,36 @@
+#include "canopy_boilerplate.h"
+#include <canopy.h>
+
+static bool on_change__speed(int8_t value)
+{
+   /* Your code here.
+    * Return true on success.
+    */
+   return false;
+}
+
+static bool on_canopy_init()
+{
+    return false;
+}
+
+static bool on_canopy_shutdown()
+{
+    return false;
+}
+
+static bool on_connected()
+{
+    return false;
+}
+
+static bool on_disconnected()
+{
+    return false;
+}
+
+static bool on_report_requested()
+{
+    return false;
+}
+
