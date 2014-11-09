d16b6823f93cfb2ae5d5d9a82143b4494269c6ed
Greg Prisament <greg@canopy.link>
2014-11-08 17:03:08 -0800

    Add st_log module (not implemented yet)

---
+++ libcanopy/src/log/st_log.h
@@ -0,0 +1,30 @@
+// Copyright 2014 SimpleThings, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ST_LOG_INCLUDED
+#define ST_LOG_INCLUDED
+
+// Logging utility library for Canopy.
+// Implemented as a simple wrapper around RedLog.
+
+// TODO: implement for realz
+#if 0
+#define st_log_trace printf
+#else
+#define st_log_trace(...) (void)0
+#endif
+
+#endif // ST_LOG_INCLUDED
+
+
